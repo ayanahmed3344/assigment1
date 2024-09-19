3359827c74dced85ae5664c224759236355dceca
ayanahmed3344 <ahmedayan4447@gmail.com>
2024-09-19 23:12:11 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,58 @@
+// chapter#1 [alert]
+// An alert is a box that pops up to give the user a message
+// alert("welcome");
+
+// capter#2 [variable for strings]
+//x = "ayan"; 
+// y = "ahmed";
+// z = x + y;
+// console.log(z)
+
+//chapter#3 [variale for strings]
+// var weigth = 34;
+// console.log(weigth + 14);
+
+// chapter#4 [legal or illegal variable]
+// legal
+// .Letters, Digits, Underscores, and Dollar Signs**: are legal variable in javascript
+// illegal
+// var 2number = 10; // Starts with a digit
+// var my variable = 'test'; // Contains a space
+// var my-var = 'test'; // Contains a hyphen
+// var function = 'test'; // Reserved word
+
+// chapter#6 [ Math expressions Familiar operators ]
+// var a = 4
+// var b = 5
+// console.log(a-b)
+// var a = 25
+// var b = 25
+// console.log(a+b)
+// var a = 6
+// var b = 9
+// console.log(a * b)
+// var a = 50
+// var b = 8
+// console.log(a / b)
+
+//capter#7 [Math expressions Unfamiliar operators ]
+// var num = 26;
+// var newNum = ++num;
+// console.log(++num)
+// var num = 26;
+// var newNum = --num;
+// console.log(--num)
+
+//chapter#8 [ Math expressions Eliminating ambiguity ]
+// var value = 6 - 1 * 4;
+// console.log(value)
+// var value = 4 +(3 * 4)
+// console.log(value)
+
+//chapter#8 [Concatenating text strings]
+// var name = "ayan"
+// var surname = "ahmed"
+// alert(name +"thanks" + surname)
+
+// chapter#9 [prompt]
+//var msg = prompt("enter your name")
